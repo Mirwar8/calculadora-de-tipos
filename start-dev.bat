6b27d080ed87ced1fa69ce9d7f9c66cabd88a1f8
Arwy Zam <arwenoras12@gmail.com>
2026-02-02 22:35:26 -0600

    start server .bat

---
+++ start-dev.bat
@@ -0,0 +1,13 @@
+@echo off
+echo ========================================
+echo   TypeCoverage - Iniciando servidor...
+echo ========================================
+echo.
+
+cd /d "%~dp0"
+
+echo Ejecutando npm run dev...
+echo.
+npm run dev
+
+pause
